e6df9f40ba1b36f5c9bea80fd1b57e8604958b18
nagysandordominik <thesatyka@gmail.com>
2022-11-23 20:50:31 +0100

    add: flipcoin and handleclick function

--- src/coin-container.js
+++ src/coin-container.js
@@ -1,23 +1,44 @@
 import { render } from "@testing-library/react";
 import React, { Component } from "react";
+import {choice} from "./helper";
 
 class Flipper extends Component {
-    static defaultProps = [
-        {side: 'heads', url: 'https://tinyurl.com/react-coin-heads-jpg'}
-        {side: 'tails', url: 'https://tinyurl.com/react-coin-tails-jpg'}
+    static defaultProps = {
+    coins:[
+        {side: "heads", imgSrc: "https://tinyurl.com/react-coin-heads-jpg"},
+        {side: "tails", imgSrc: "https://tinyurl.com/react-coin-tails-jpg"}
     ]
+};
+    constructor(props){
     super(props);
     this.state = {
         currCoin: null,
         nFlips: 0,
         nHeads: 0,
         nTails: 0
-    };
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+    flipCoin() {
+        const newCoin = choice(this.props.coins);
+        this.setState(st => {
+            return {
+                currCoin: newCoin,
+                nFlips: st.nFlips + 1
+            };
+        });
+    }
+    handleClick(e) {
+        this.flipCoin();
+    }
     render(){
         return (
             <div className='flipper'>
                 <h2>Flip a coin!</h2>
-                <p>out of {this.state.nFlips} flips, there have been {this.state.nHeads} heads, and {this.state.nTails}tails</p>
+                <button onClick={this.handleClick}>Flip it</button>
+                <p>Out of {this.state.nFlips} flips,
+                there have been {this.state.nHeads}{" "} heads,
+                and {this.state.nTails} tails</p>
       </div>
         )
     }
